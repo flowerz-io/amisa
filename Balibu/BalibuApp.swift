df0de9cf0ac791eedb339b44a72ca9d0477d9cfa
JonasLeGall <legall.jonas@gmail.com>
2026-03-11 12:09:39 +0100

    Initial Commit

---
+++ Balibu/BalibuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalibuApp.swift
+//  Balibu
+//
+//  Created by Jonas Le Gall on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BalibuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalibuTests/BalibuTests.swift
@@ -0,0 +1,17 @@
+//
+//  BalibuTests.swift
+//  BalibuTests
+//
+//  Created by Jonas Le Gall on 11/03/2026.
+//
+
+import Testing
+@testable import Balibu
+
+struct BalibuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
